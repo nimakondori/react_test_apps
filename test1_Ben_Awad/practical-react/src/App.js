092a60ae380ea5f63133d12865d21c1ad383d9cd
Nima <nimakondori96@gmail.com>
2020-09-19 17:29:23 -0700

    second commit'

--- test1_Ben_Awad/practical-react/src/App.js
+++ test1_Ben_Awad/practical-react/src/App.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+// const Body = () => (
+//   <p className="App_body">
+//     To get started, edit the <code>src/App.js </code> and save and reload
+//   </p>
+// );
+const Body = () => {
+  return (
+    <p className="App_body">
+      <h3>
+        To get started, edit the <code>src/App.js </code> and save and reload
+      </h3>
+      <h4>
+        <a href="https://google.com">This is the second version</a>
+      </h4>
+    </p>
+  );
+};
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <Body />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          why the fuck is this here
         </a>
       </header>
     </div>
